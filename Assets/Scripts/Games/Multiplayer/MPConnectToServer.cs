7756f9366f8590aef2de9e170751bf6a25bb45a7
Amin Hanif <aminhanif24@gmail.com>
2023-03-21 01:06:31 +0700

    Changelog
    + Sucessfully AssetBundle (Android)

--- Assets/Scripts/Games/Multiplayer/MPConnectToServer.cs
+++ Assets/Scripts/Games/Multiplayer/MPConnectToServer.cs
@@ -1,10 +1,14 @@
 using System;
 using System.Linq;
 using System.Text;
+using System.Collections;
 using TMPro;
 using Photon.Pun;
 using Photon.Realtime;
 using Hashtable = ExitGames.Client.Photon.Hashtable;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 
 namespace MetaversePrototype.Game
@@ -12,6 +16,9 @@ namespace MetaversePrototype.Game
     public class MPConnectToServer : MonoBehaviourPunCallbacks
     {
         public TMP_InputField textInputField;
+        public Slider progressBar;
+        public TextMeshProUGUI progressText;
+        public bool useAssetBundle = true;
 
         #region Photon Settings
         private const byte maxPlayer = 4;
@@ -32,8 +39,6 @@ namespace MetaversePrototype.Game
         public override void OnConnectedToMaster()
         {
             base.OnConnected();
-
-            UpdateDebugText("Connected to the master server.");
             
             RoomOptions option = new RoomOptions();
             option.MaxPlayers = maxPlayer;
@@ -59,15 +64,48 @@ namespace MetaversePrototype.Game
             base.OnJoinedRoom();
 
             UpdateDebugText("Joining a room..");
-            PhotonNetwork.LoadLevel("Prototype Demo");
+            
+            if(useAssetBundle){
+                StartCoroutine(LoadBundles());
+            } else {
+                // Dont forget to enable scene in build!
+                PhotonNetwork.LoadLevel("Prototype Demo");
+            }
+        }
+        protected const string url = "https://firebasestorage.googleapis.com/v0/b/metaverse-prototype.appspot.com/o/prototype%20demo?alt=media&token=61ef6b4c-387a-44d1-b37b-9189b851dd45";
+        protected const string bundleName = "prototype demo";
+        IEnumerator LoadBundles(){
+    
+            UnityWebRequest www = UnityWebRequestAssetBundle.GetAssetBundle(url);
+            StartCoroutine(WaitForResponse(www));
+            yield return www.SendWebRequest();
+    
+            if (www.result != UnityWebRequest.Result.Success) {
+                UpdateDebugText("Failed joining a room.");
+            }
+            else {
+                AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(www);
+                string[] scenePaths = bundle.GetAllScenePaths();
+                PhotonNetwork.LoadLevel(scenePaths[0]);
+            }
+        }
 
+        IEnumerator WaitForResponse(UnityWebRequest request)
+        {
+            while (!request.isDone)
+            {
+                // print(request.downloadProgress);
+                progressBar.value = request.downloadProgress;
+                progressText.text = (request.downloadProgress * 100).ToString("0") + "%";
+                yield return new WaitForSeconds(0.2f);
+            }
         }
 
         public void UpdateDebugText(string text){
             StringBuilder sb = new StringBuilder();
             sb.Append(textInputField.text);
             sb.Append(text);
-            textInputField.text = sb.ToString() + Environment.NewLine;
+            textInputField.text = sb.ToString() + Environment.NewLine + Environment.NewLine;
         }
     }    
 }
